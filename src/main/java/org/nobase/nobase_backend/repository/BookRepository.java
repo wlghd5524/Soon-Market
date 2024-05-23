571155711120778b2b6267b4fbe38f5de2ffd99a
kmeca2504 <heelo.git.kmeca2504@naver.com>
2024-05-23 19:46:10 +0900

    hi

--- src/main/java/org/nobase/nobase_backend/repository/BookRepository.java
+++ src/main/java/org/nobase/nobase_backend/repository/BookRepository.java
@@ -4,6 +4,9 @@ import org.nobase.nobase_backend.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
+
+
